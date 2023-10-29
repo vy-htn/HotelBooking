d02d7626487253351b99c9a1e609f0feb0e23568
Vy Huỳnh Thị Nhật <21522810@gm.uit.edu.vn>
2023-10-30 02:00:48 +0700

    delete button select room

--- selectroom.js
+++ selectroom.js
@@ -6,44 +6,40 @@ document.getElementById('toggle').addEventListener('click', function() {
         guests.style.display = 'none';
     }
 });
+var roomCount = 1;
 function addRoom() {
-    // Get the guests div
-    var guests = document.getElementById('guests');
+    var table = document.getElementById('table');
 
-    // Get the room-buttons div
-    var roomButtons = document.querySelector('.room-buttons');
+    var newRow = document.createElement('tr');
+    var addButton = document.querySelector('button[onclick="addRoom()"]');
+    roomCount++;
+    
+    if (roomCount === 10) {
+        addButton.disabled = true;
+    }
 
-    // Create a new room div
-    var newRoom = document.createElement('div');
-    newRoom.className = 'room';
-
-    // Add guest types to the new room
-    // Note: You'll need to replace 'n' with the actual room number
-    newRoom.innerHTML = `
-        <div class="guest-type">
-            <div id="container">
-                <button>-</button>
-                <input type="text" id="guest-number" name="adults1">
-                <button>+</button>
-            </div>
-        </div>
-        <div class="guest-type">
-            <div id="container">
-                <button>-</button>
-                <input type="text" id="guest-number" name="children1">
-                <button>+</button>
-            </div>
-        </div>
-        <div class="guest-type">
-            <div id="container">
-                <button>-</button>
-                <input type="text" id="guest-number" name="infants1">
-                <button>+</button>
-            </div>
-        </div>
+    newRow.innerHTML = `
+        <td><div class="room-label"><label>Room `+roomCount+` : </label></div></td>
+        <td><div class="guest-type"><div id="container"><button>-</button><input type="text" class="guest-number" name="adults1"><button>+</button></div></div></td>
+        <td><div class="guest-type"><div id="container"><button>-</button><input type="text" class="guest-number" name="children1"><button>+</button></div></div></td>
+        <td><div class="guest-type"><div id="container"><button>-</button><input type="text" class="guest-number" name="infants1"><button>+</button></div></div></td>
+        <td><button class="delete-room"  style="background: none; border: none;"> <i class="fa-solid fa-trash" style="color: #0c84d3;"></i></button></td>
     `;
 
-    // Add the new room to the guests div before the room-buttons div
-    guests.insertBefore(newRoom, roomButtons);
+    table.appendChild(newRow);
+    newRow.querySelector('.delete-room').addEventListener('click', function() {
+        table.removeChild(newRow);
+
+        roomCount--;
+
+        if (roomCount < 10){
+            addButton.disabled = false;
+        }
+        var rooms = document.querySelectorAll('.room-label');
+        for (var i = 0; i < rooms.length; i++) {
+            rooms[i].textContent = 'Room ' + (i + 1) + ' : ';
+        }
+    });
 }
+
 
