223ace81dbb386a372e4ac629b17972c787718bf
Vy Huỳnh Thị Nhật <21522810@gm.uit.edu.vn>
2023-11-16 10:43:02 +0700

    stryker

---
+++ stryker.conf.js
@@ -0,0 +1,10 @@
+module.exports = function(config) {
+  config.set({
+    mutator: "javascript",
+    packageManager: "npm",
+    reporters: ["clear-text", "progress"],
+    testRunner: "jest",
+    transpilers: [],
+    coverageAnalysis: "off"
+  });
+};
